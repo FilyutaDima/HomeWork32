4ecbc2f9a440bb7e8823b0b356755b19129663fb
Dmitry <filyuta.dima@yandex.ru>
2022-04-03 23:02:43 +0300

    Project created

---
+++ HomeWork32/HomeWork32/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork32
+//
+//  Created by Dmitry on 3.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork32/HomeWork32/HomeWork32App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork32App.swift
+//  HomeWork32
+//
+//  Created by Dmitry on 3.04.22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
